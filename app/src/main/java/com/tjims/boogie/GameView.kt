0cd8339deff9f093f31494773eb9ed09fa94eb38
tjimeno <tjimeno1@gmail.com>
2021-12-09 18:56:30 -0700

    init

---
+++ app/src/main/java/com/tjims/boogie/GameView.kt
@@ -0,0 +1,31 @@
+package com.tjims.boogie
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class GameView(context: Context, attributes: AttributeSet) : SurfaceView(context, attributes), SurfaceHolder.Callback {
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+    fun update() {
+
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+    }
+
+}
---
+++ app/src/main/java/com/tjims/boogie/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.tjims.boogie
+
+import android.app.Activity
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
