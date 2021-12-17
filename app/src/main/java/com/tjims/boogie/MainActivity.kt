f126291e81d7c8d0ce815a1a6a999f44fac5f96e
tjimeno <tjimeno1@gmail.com>
2021-12-16 21:15:41 -0700

    Layout stuff

--- app/src/main/java/com/tjims/boogie/MainActivity.kt
+++ app/src/main/java/com/tjims/boogie/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.tjims.boogie
 
-import android.app.Activity
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
